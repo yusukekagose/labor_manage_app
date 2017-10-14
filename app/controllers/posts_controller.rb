179b81d2f086a1fac1488cfbe1ef20e938e2edfd
kagose yusuke <>
2017-10-14 12:07:15 +0900

    user association with post test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,12 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+
   end
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
 
     if @post.save
       redirect_to @post, notice: 'Your post was created successfully'
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,6 +15,9 @@ describe 'navigate' do
 
   describe 'creation' do
     before do
+      user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+      login_as(user, :scope => :user)
+      visit new_post_path
       visit new_post_path
     end
 
@@ -28,6 +31,14 @@ describe 'navigate' do
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
+    end
+
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 
