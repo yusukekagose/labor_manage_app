6abb3e7da91b8897b0b01694e0f5ceb150a56b35
kagose yusuke <>
2017-10-17 20:51:29 +0900

    fix seed file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,6 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.assets.debug = true
@@ -23,6 +21,10 @@ Rails.application.configure do
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 end
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -15,4 +15,17 @@ namespace :notification do
     # all characters have to be a number
   end
 
+  desc "Sends mail notification to managers (admin users) each day to inform of pending overtime requests"
+  task manager_email: :environment do
+    submitted_posts = Post.submitted
+    admin_users = AdminUser.all
+
+    if submitted_posts.count > 0
+      admin_users.each do |admin|
+        ManagerMailer.email(admin).deliver_now
+      end
+    end
+  end
+
+
 end
