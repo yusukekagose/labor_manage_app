73eea638f8dd0571793e7b77ba59eedc34bb8583
kagose yusuke <>
2017-10-18 10:19:01 +0900

    before update callback of end_date of auditlog

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -5,8 +5,14 @@ class AuditLog < ApplicationRecord
   validates_presence_of :user_id, :status, :start_date
   after_initialize :set_defaults
 
+  before_update :set_end_date, if: :confirmed?
+
 
   private
+
+    def set_end_date
+      self.end_date = Data.today
+    end
     def set_defaults
       self.start_date ||= Date.today - 6.days
     end
